4e1257c17a2899c83e39457f7f16f92ce511ef0a
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-18 14:59:38 +0100

    HomePage component

---
+++ src/Homepage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="contenr">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="contenr">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="contenr">
+            <h1 className="title">SHOES</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="contenr">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="contenr">
+            <h1 className="title">MENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
