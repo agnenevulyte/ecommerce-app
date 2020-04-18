b7286edb7063172ed85e3150e1fe01d6e2555c69
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-18 15:09:01 +0100

    added homepage.scss

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
+import './homepage.scss';
 
 export default function Homepage() {
   return (
     <div className="homepage">
       <div className="directory-menu">
         <div className="menu-item">
-          <div className="contenr">
+          <div className="content">
             <h1 className="title">HATS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
 
         <div className="menu-item">
-          <div className="contenr">
+          <div className="content">
             <h1 className="title">JACKETS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
 
         <div className="menu-item">
-          <div className="contenr">
+          <div className="content">
             <h1 className="title">SHOES</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
 
         <div className="menu-item">
-          <div className="contenr">
+          <div className="content">
             <h1 className="title">WOMENS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
 
         <div className="menu-item">
-          <div className="contenr">
+          <div className="content">
             <h1 className="title">MENS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
