d3681dbc4cb241bc3d7a6cdf91cfb29b0a9a651b
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-19 19:12:23 +0100

    filter items in CollectionPreview

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './collection-preview.styles.scss';
 
+// if items array gets too big, it's going to influence the performance and slow the website down
+// the CollectionPreview component gets rendered, the filter/map function call gets called every time
 export default function CollectionPreview({title, items}) {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.map((item) => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <div key={item.id}>{item.name}</div>
+          ))}
       </div>
     </div>
   );
