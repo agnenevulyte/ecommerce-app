63c604d7afe57efdf6d239f793ef73aa36b7e7b2
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-19 21:25:14 +0100

    created SignInAndSignUp page

---
+++ src/pages/sign-in-and-sign-up/SignInAndSignUoPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/SignIn';
+
+export default function SignInAndSignUoPage() {
+  return (
+    <div className="sign-in-and-sign-up">
+      SIGN IN
+      <div>
+        <SignIn />
+      </div>
+    </div>
+  );
+}
