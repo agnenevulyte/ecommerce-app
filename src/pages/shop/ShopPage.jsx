33f67b2501895841a72aaf39ebc431827fae0ad8
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-19 19:03:59 +0100

    spreading  to  {id, ...otherCollectionProps}

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -8,17 +8,14 @@ export default class ShopPage extends Component {
     this.state = {
       collections: SHOP_DATA,
     };
-    console.log(this.state.collections);
+    // console.log(this.state.collections);
   }
   render() {
     const {collections} = this.state;
     return (
       <div className="shop-page">
-        {collections.map(({collection}) => (
-          <CollectionPreview
-            title={collection.title}
-            items={collection.items}
-          />
+        {collections.map(({id, ...otherCollectionProps}) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
     );
