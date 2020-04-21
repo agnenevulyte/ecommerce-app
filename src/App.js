6ee7f29e3088724d3e8be1485a1e27f885bcc5f8
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-21 22:42:19 +0100

    import auth to App.js and check the user

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUoPage';
+import {auth} from './firebase/firebase.utils';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+export default class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({currentUser: user});
+      console.log('cur---', user);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
