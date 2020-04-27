8bb479215069ee067e690f08fa67570f08e60feb
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-27 11:40:03 +0100

    return state in cartRducer instead of initialState

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -10,12 +10,12 @@ const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
-        ...initialState,
+        ...state,
         hidden: !state.hidden,
       };
     case CartActionTypes.ADD_ITEM:
       return {
-        ...initialState,
+        ...state,
         // action.payload is the item we want to add
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
