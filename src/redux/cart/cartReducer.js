6aec4777ef77e4eea9c47ec3ab5f4c36225d3d4b
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-30 20:32:20 +0100

    removing items from checkout page. 1. created action, updated reducer, dispatched an action in CheckoutItem component

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -19,6 +19,13 @@ const cartReducer = (state = initialState, action) => {
         // action.payload is the item we want to add
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
     default:
       return state;
   }
