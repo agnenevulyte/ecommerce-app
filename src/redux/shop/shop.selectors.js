992d65a729aca5fe90c2dcc0f392a485aef9d312
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-05-02 21:14:08 +0100

    created respective selectors and updatetd coresponding component with new redox flow with directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,15 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+// that's how  we map through objects. 1. get the keys 2. map through keys
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
