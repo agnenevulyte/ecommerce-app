e93225f9d552cd6c7902b36021359072647f5d8e
agnenevulyte <you@example.com>
2020-05-16 14:35:12 +0100

    created a HOC WithSpinner, updated how we get collections from firestore, removed shop.data locally

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -7,8 +7,16 @@ import CollectionPage from '../collection/CollectionPage';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
 import { updateCollections } from '../../redux/shop/shopActions';
+import WithSpinner from '../../components/with-spinner/WithSpinner';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+	state = {
+		loading: true
+	};
+
 	unsubscribeFromSnapshot = null;
 
 	componentDidMount() {
@@ -23,15 +31,24 @@ class ShopPage extends React.Component {
 			console.log('snapShot-----', snapshot);
 			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
 			updateCollections(collectionsMap);
+			this.setState({ loading: false });
 		});
 	}
 
 	render() {
 		const { match } = this.props;
+		const { loading } = this.state;
 		return (
 			<div className="shop-page">
-				<Route exact path={`${match.path}`} component={CollectionsOverview} />
-				<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+				<Route
+					exact
+					path={`${match.path}`}
+					render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}
+				/>
+				<Route
+					path={`${match.path}/:collectionId`}
+					render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
+				/>
 			</div>
 		);
 	}
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,20 +1,14 @@
-import {createSelector} from 'reselect';
+import { createSelector } from 'reselect';
 
 const selectShop = (state) => state.shop;
 
-export const selectCollections = createSelector(
-  [selectShop],
-  (shop) => shop.collections
-);
+export const selectCollections = createSelector([ selectShop ], (shop) => shop.collections);
 
 // that's how  we map through objects. 1. get the keys 2. map through keys
 export const selectCollectionsForPreview = createSelector(
-  [selectCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+	[ selectCollections ],
+	(collections) => (collections ? Object.keys(collections).map((key) => collections[key]) : [])
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
-  );
+	createSelector([ selectCollections ], (collections) => (collections ? collections[collectionUrlParam] : null));
