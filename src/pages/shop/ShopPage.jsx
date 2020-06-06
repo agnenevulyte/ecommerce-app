d3ae208a29f42c8ffe17ae4561389d18b40796d5
Agne Nevulyte <agnenevulyte@Agnes-Air.connect>
2020-06-06 15:26:43 +0100

    updating code so shopPage just routes and logic is transfered to Containers

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,61 +1,30 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
-import CollectionPage from '../collection/CollectionPage';
-
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-
-import { updateCollections } from '../../redux/shop/shopActions';
-import WithSpinner from '../../components/with-spinner/WithSpinner';
-
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import { fetchCollectionsStartAsync } from '../../redux/shop/shopActions';
+import CollectionsOverviewContainer from '../../components/collections-overview/CollectionsOverviewContainer';
+import CollectionPageContainer from '../../pages/collection/CollectionPageContainer'
 
 class ShopPage extends React.Component {
-	state = {
-		loading: true
-	};
-
-	unsubscribeFromSnapshot = null;
 
 	componentDidMount() {
-		const { updateCollections } = this.props;
-		// the name in firestore is in the ''
-		const collectionRef = firestore.collection('collections');
-
-		// whenever the collectionRef updates or runs for the first time this collectionRef
-		// will send us a snapshot representing the object of the collections array
-		// at the time when this code renders
-		// .get() makes an API call to fetch back the data associated to the collectionRef
-		collectionRef.get().then((snapshot) => {
-			console.log('snapShot-----', snapshot);
-			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-			updateCollections(collectionsMap);
-			this.setState({ loading: false });
-		})
-
-		// or we can use fetch
-		// fetch('https://firestore.googleapis.com/v1/projects/crwn-db-23cfc/databases/(default)/documents/collections')
-		// .then(response => response.json())
-		// .then(collections => console.log('fetch collections-------', collections))
+		const { fetchCollectionsStartAsync } = this.props;
+		fetchCollectionsStartAsync();		
 	}
-	
 	
 
 	render() {
 		const { match } = this.props;
-		const { loading } = this.state;
 		return (
 			<div className="shop-page">
 				<Route
 					exact
 					path={`${match.path}`}
-					render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}
+					component={CollectionsOverviewContainer}
 				/>
 				<Route
 					path={`${match.path}/:collectionId`}
-					render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
+					component={CollectionPageContainer}
 				/>
 			</div>
 		);
@@ -63,7 +32,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-	updateCollections: (collectionsMap) => dispatch(updateCollections(collectionsMap))
+	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
